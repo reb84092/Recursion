27d95e3f9893ff06a0bfde3af81feffe851003f0
reb84092 <reb84092@gmail.com>
2016-03-09 15:19:57 -0700

    Added stuff to the recursion frame

---
+++ src/recursion/view/RecursionFrame.java
@@ -0,0 +1,10 @@
+package recursion.view;
+
+import javax.swing.JFrame;
+import recursion.controller.*;
+
+public class RecursionFrame extends JFrame
+{
+	private RecursionController baseController;
+	private RecursionPanel basePanel;
+}
