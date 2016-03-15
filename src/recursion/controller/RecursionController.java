ac114325f9583d1cf9f7999d634465b86cefac33
reb84092 <reb84092@gmail.com>
2016-03-15 07:53:29 -0600

    Made the base frame

--- src/recursion/controller/RecursionController.java
+++ src/recursion/controller/RecursionController.java
@@ -1,11 +1,18 @@
 package recursion.controller;
 
+import recursion.view.RecursionFrame;
+
 public class RecursionController
 {
+	private RecursionFrame baseFrame;
+	
+	
 	public RecursionController()
 	{
-		
+	baseFrame = new RecursionFrame(this);
+	
 	}
+	
 	public void start()
 	{
 		
--- src/recursion/view/RecursionFrame.java
+++ src/recursion/view/RecursionFrame.java
@@ -7,4 +7,8 @@ public class RecursionFrame extends JFrame
 {
 	private RecursionController baseController;
 	private RecursionPanel basePanel;
+	public RecursionFrame(RecursionController baseController)
+	{
+		
+	}
 }
